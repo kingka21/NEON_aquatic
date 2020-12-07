683d3294d572a47ceeec0fdabfedaeab39e07c58
Jennifer Edmonds <jennifer.edmonds@nsc.edu>
2020-12-07 12:54:57 -0800

    Changes due to transformation and checking for homogeneity of variance.

--- Code/02_PCA_analysis_JWEedits.R
+++ Code/02_PCA_analysis_JWEedits.R
@@ -2,9 +2,16 @@
 #Created by KK  March 30, 2020
 #Modified 6Aug2020 by JWEdmonds, 20Sep20  ## 
 #modified by KK on 13Sep2020; 28Sep 2020, Oct 5, 2020 
+#modified by JWE on 7Dec2020
 
 
 install.packages("factoextra") #install the package if you have not used it before
+install.packages(fmsb)
+install.packages("moments")
+install.packages("rcompanion")
+install.packages("REdaS")
+install.packages("parameters")
+install.packages("RNHANES")
 library(factoextra) #library for great visualization of PCA results
 library(FactoMineR) #library to perfomr PCA and HCPC clusters
 library(ggplot2)
@@ -17,6 +24,15 @@ library(ggsci)
 library(gridExtra)
 install.packages("psych")
 library(psych)
+library(fmsb)
+library(ggpubr)
+library(moments)
+library(rcompanion)
+library(REdaS)
+library(parameters)
+library(car)
+library(tidyverse)
+library(RNHANES)
 ###################run PCA for surface water data with all sites #########################
 
 #bring in most recent data, QAQC and all sites updated Sep 28 
@@ -25,6 +41,287 @@ SWgrab_allsites_dat<-select(sw_all, -c(sampleID, collectDate, pH, TDS, Br)) %>% 
                      data.table:: setnames(old=c("NH4...N", "NO3.NO2...N", "initialSamplepH"), new=c("NH4N", "NO3NO2N", "pH"))
 
 summary(SWgrab_allsites_dat$siteID)
+
+#Remove rows with na
+SWgrab_allsites_dat<-na.omit(SWgrab_allsites_dat) # rows where lab pH was not available (from 1504 to 1385)
+
+#Remove one crazy outlier value with a "bad" MN concentration
+SWgrab_allsites_dat<-(SWgrab_allsites_dat [-245, ])
+
+
+#Test of normality for each paramenter using Shapiro-Wilks test
+shapiro.test(SWgrab_allsites_dat$Ca)
+shapiro.test(SWgrab_allsites_dat$Cl)
+shapiro.test(SWgrab_allsites_dat$DIC)
+shapiro.test(SWgrab_allsites_dat$DOC)
+shapiro.test(SWgrab_allsites_dat$F)
+shapiro.test(SWgrab_allsites_dat$Fe)
+shapiro.test(SWgrab_allsites_dat$K)
+shapiro.test(SWgrab_allsites_dat$Mg)
+shapiro.test(SWgrab_allsites_dat$Mn)
+shapiro.test(SWgrab_allsites_dat$Na)
+shapiro.test(SWgrab_allsites_dat$NH4N)
+shapiro.test(SWgrab_allsites_dat$NO3NO2N)
+shapiro.test(SWgrab_allsites_dat$Si)
+shapiro.test(SWgrab_allsites_dat$SO4)
+shapiro.test(SWgrab_allsites_dat$TDP)
+shapiro.test(SWgrab_allsites_dat$pH)
+
+
+#check data for skewness
+skewness(SWgrab_allsites_dat, na.rm = TRUE)
+
+#transform data and create a column for the transformed data
+
+#Q-Q Plot is bent at top after transformation
+SWgrab_allsites_dat$transCa<-transformTukey(
+  SWgrab_allsites_dat$Ca,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q Plot is great after transformation
+SWgrab_allsites_dat$transCl<-transformTukey(
+  SWgrab_allsites_dat$Cl,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q Plot is bent at top after transformation
+SWgrab_allsites_dat$transDIC<-transformTukey(
+  SWgrab_allsites_dat$DIC,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+
+#Q-Q Plot is great after transformation
+SWgrab_allsites_dat$transDOC<-transformTukey(
+  SWgrab_allsites_dat$DOC,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+
+#Q-Q Plot is bent at bottom, badly, after transformation
+SWgrab_allsites_dat$transF<-transformTukey(
+  SWgrab_allsites_dat$F,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q Plot is bent at bottom, badly, after transformation
+SWgrab_allsites_dat$transFe<-transformTukey(
+  SWgrab_allsites_dat$Fe,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q Plot is bent at bottom a little after transformation
+SWgrab_allsites_dat$transK<-transformTukey(
+  SWgrab_allsites_dat$K,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q Plot is bent at top, badly, after transformation
+SWgrab_allsites_dat$transMg<-transformTukey(
+  SWgrab_allsites_dat$Mg,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q Plot is bent badly at bottom after transformation
+SWgrab_allsites_dat$transMn<-transformTukey(
+  SWgrab_allsites_dat$Mn,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q plot looks good after transformation
+SWgrab_allsites_dat$transNa<-transformTukey(
+  SWgrab_allsites_dat$Na,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q plot looks good after transformation
+SWgrab_allsites_dat$transNH4N<-transformTukey(
+  SWgrab_allsites_dat$NH4N,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q plot looks OK, bent a little at top after transformation
+SWgrab_allsites_dat$transNO3NO2N<-transformTukey(
+  SWgrab_allsites_dat$NO3NO2N,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q plot looks good after transformation
+SWgrab_allsites_dat$transSi<-transformTukey(
+  SWgrab_allsites_dat$Si,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q plot looks good after transformation
+SWgrab_allsites_dat$transSO4<-transformTukey(
+  SWgrab_allsites_dat$SO4,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+#Q-Q plot looks bent at bottom after transformation
+SWgrab_allsites_dat$transTDP<-transformTukey(
+  SWgrab_allsites_dat$TDP,
+  start = -10,
+  end = 10,
+  int = 0.025,
+  plotit = TRUE,
+  verbose = FALSE,
+  quiet = FALSE,
+  statistic = 2,
+  returnLambda = FALSE
+)
+
+
+#visual assessment of frequency of data binned
+plotNormalHistogram(SWgrab_allsites_dat$transFe)
+
+#check data for skewness in transformed data
+skewness(SWgrab_allsites_dat, na.rm = TRUE)
+
+#create dataframe with just values for transformed data
+SWgrab_allsites_dat_trans<-select(SWgrab_allsites_dat, -c(Ca, Cl, DIC, DOC, F, Fe, K, Mg, Mn, Na, NH4N, NO3NO2N, Si, SO4, TDP))
+SWgrab_allsites_dat_trans_noFFeMnTDP<-select(SWgrab_allsites_dat, -c(Ca, Cl, DIC, DOC, F, Fe, K, Mg, Mn, Na, NH4N, NO3NO2N, Si, SO4, TDP, transF, transFe, transMn, transTDP))
+write.csv(SWgrab_allsites_dat_trans, 'Data/surface_water_TRANSPOSE.csv', row.names = FALSE)
+
+#run the Kaiser-Meyer-Olkin (KMO) Test
+#one method, checking both transformed and untransformed data
+check_kmo(SWgrab_allsites_dat [, -1])
+check_kmo(SWgrab_allsites_dat_trans [, -1])
+check_kmo(SWgrab_allsites_dat_trans_noFFeMnTDP [, -1])
+
+
+#Bartlett's Test Of Sphericity
+bart_spher(SWgrab_allsites_dat_trans [,-1], use = c("pairwise.complete.obs"))
+bartlett.test(SWgrab_allsites_dat_trans [,-1])
+
+bart_spher(SWgrab_allsites_dat_trans_noFFeMnTDP [,-1], use = c("pairwise.complete.obs"))
+bartlett.test(SWgrab_allsites_dat_trans_noFFeMnTDP [,-1])
+
+
+#convert values to z-scores just to test influence on homogeneity of variance
+sw_transpose_scale<-scale(SWgrab_allsites_dat_trans)
+#export scaled data to change orientation of data in Excel, outside of R Statistic
+write.csv(SWgrab_allsites_dat_trans_scale,'Data/sw_transpose_scale.csv', row.names = FALSE)
+
+#bring in transformed data in long format, where "reduced" means F, Fe, Mn, and TDP was eliminated, and "scale" means data were converted to zscores
+sw_transpose_scale<-read.csv('Data/sw_transpose_scale.csv', header = TRUE)
+sw_transpose_scale_reduced<-read.csv('Data/sw_transpose_scale_reduced.csv', header = TRUE)
+sw_transpose<-read.csv('Data/surface_water_TRANSPOSED_AllVariables.csv', header = TRUE)
+sw_transpose_reduced<-read.csv('Data/surface_water_TRANSPOSED.csv', header = TRUE)
+
+#Levene's Test and Fligner-Killeen test of homogeneity of variance on imported dataset
+leveneTest(analyteConcentration ~ analyte, sw_transpose_scale_reduced)
+fligner.test(analyteConcentration ~ analyte, sw_transpose_scale_reduced)
+
+#visualization of scatter in datasets
+ggplot(sw_transpose_scale, aes(x = analyte, y = analyteConcentration)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  ggtitle("Transformed and Scaled values for each analyte")
+
+#Visual representation of variable's distribution
+ggdensity(SWgrab_allsites_dat_trans, x = "transTDP", fill = "lightgray", title = "Fe") +
+  scale_x_continuous() +
+  stat_overlay_normal_density(color = "red", linetype = "dashed")
 
 #keep all sites - they all have >20 obs! 
 #sites_w_data<- dplyr::filter(SWgrab_allsites_dat, 
@@ -41,31 +338,35 @@ summary(SWgrab_allsites_dat$siteID)
                     
 
 #*run PCA ####
-SWgrab_allsites_dat<-na.omit(SWgrab_allsites_dat) # rows where lab pH was not available (from 1504 to 1385)
-allsites.pca<-prcomp(SWgrab_allsites_dat[, -1], scale=TRUE) #perform PCA without the siteID 
+allsites.pca_trans<-prcomp(SWgrab_allsites_dat_trans [, -1], scale=TRUE) #perform PCA without the siteID and without line 268, a CUPE site with outlier Mn value
+
+#alternative PCA command with retx=TRUE
+allsites.pca_trans_ROTATE<-prcomp(SWgrab_allsites_dat_trans[, -1], retx=TRUE, scale=TRUE)
 
 #rotation 
-rawLoadings     <- allsites.pca$rotation[,1:3] %*% diag(allsites.pca$sdev, 3, 3)
+rawLoadings     <- allsites.pca$rotation[,1:4] %*% diag(allsites.pca$sdev, 4, 4)
 rotatedLoadings <- varimax(rawLoadings)$loadings
-scores <- scale(allsites.pca$x[,1:3]) %*% varimax(rawLoadings)$rotmat
+scores <- scale(allsites.pca$x[,1:4]) %*% varimax(rawLoadings)$rotmat
 print(scores[1:5,])  
 
 #eigenvalues
-fviz_eig(allsites.pca)  ###eigen values associated with each PC (how much does each explain the variation in the data)
-eig.val<-get_eigenvalue(allsites.pca) ## table of the eigenvalues 
+fviz_eig(allsites.pca_trans)  ###eigen values associated with each PC (how much does each explain the variation in the data)
+eig.val_trans<-get_eigenvalue(allsites.pca_trans) ## table of the eigenvalues 
 
 # graph results with the site grouping, all variables 
-fviz_pca_ind(allsites.pca, label="none", 
-                                 habillage=SWgrab_allsites_dat$siteID,  # group by the site
+fviz_pca_ind(allsites.pca_trans, label="none", 
+                                 habillage=SWgrab_allsites_dat_trans$siteID,  # group by the site
              addEllipses=TRUE, ellipse.level=0.75)
 #PCAallsites_allvar +ylim(-4,5)+xlim(-7.5, 4.0) #adjust axis if needed
 
 #graph vectors of PCA
-vectors_SW<-fviz_pca_var(allsites.pca,
+vectors_SW<-fviz_pca_var(allsites.pca_trans,
              col.var = "contrib", # Color by contributions to the PC
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
              repel = TRUE, 
-             title = "Surface water")     # Avoid text overlapping
+             title = "Surface Water PCA")     # Avoid text overlapping
+
+vectors_SW
 
 #shows the top variables contributing to the 1st and 2nd axis
 #red dotted line shows expected average contribution. 
@@ -97,16 +398,16 @@ var_coord_func <- function(loadings, comp.sdev){
   loadings*comp.sdev
 }
 
-loadings <- allsites.pca$rotation
-sdev <- allsites.pca$sdev
+loadings <- allsites.pca_trans$rotation
+sdev <- allsites.pca_trans$sdev
 var.coord <- t(apply(loadings, 1, var_coord_func, sdev)) 
 print(round(var.coord[, 1:3], 3))
 
 #*HCPC cluster analysis ####
-pca_result<-PCA(SWgrab_allsites_dat[, -1], scale.unit = TRUE, graph=FALSE) # then data are scaled to unit variance
-allsites.hc<-HCPC(pca_result, graph = TRUE) #can click on where to cut graph
+pca_result_trans<-PCA(SWgrab_allsites_dat_trans[, -1], scale.unit = TRUE, graph=FALSE) # then data are scaled to unit variance
+allsites.hc_trans<-HCPC(pca_result_trans, graph = TRUE) #can click on where to cut graph
 
-fviz_dend(allsites.hc, 
+fviz_dend(allsites.hc_trans, 
           show_labels = FALSE,                     # Label size
           palette = "jco",               # Color palette see ?ggpubr::ggpar
           rect = TRUE, rect_fill = TRUE, # Add rectangle around groups
@@ -115,7 +416,7 @@ fviz_dend(allsites.hc,
 )
 
 #make a color pallete 
-fviz_cluster(allsites.hc,
+fviz_cluster(allsites.hc_trans,
              repel = FALSE,            #  label overlapping
              show.clust.cent = TRUE, # Show cluster centers
              palette = "jco",         # Color palette see ?ggpubr::ggpar
@@ -123,22 +424,27 @@ fviz_cluster(allsites.hc,
 )
 
 ## clust number is assigned to each observation 
-clusters<-allsites.hc$data.clust
-clusters$siteID<-SWgrab_allsites_dat$siteID #add back in the siteID to see what sites are in each cluster
+clusters_trans<-allsites.hc_trans$data.clust
+clusters_trans$siteID<-SWgrab_allsites_dat_trans$siteID #add back in the siteID to see what sites are in each cluster
 
 ##look at the variables that explain clustering the most 
-allsites.hc$desc.var$quanti
+allsites.hc_trans$desc.var$quanti
+
+##look at the axes that clusters fall on
+allsites.hc_trans$desc.axes$quanti
+
 ##look at some sites within each cluster, for each cluster, the top 5 closest individuals to the cluster center is shown
-allsites.hc$desc.ind$para
+allsites.hc_trans$desc.ind$para
 
 #find out what sites are in each cluster
-frequency_clust<-rename(count(clusters, clust, siteID), Freq = n)
+frequency_clust_trans<-rename(count(clusters_trans, clust, siteID), Freq = n)
+
 
 #new figure 
-fviz_pca_ind(allsites.pca, label="none", 
-             habillage=clusters$clust,  # group by cluster
+fviz_pca_ind(allsites.hc_trans, label="none", 
+             habillage=clusters_trans$clust,  # group by cluster
              addEllipses=FALSE ,
-             palette = c("darkblue", "#F6Be00",  "#552586", "grey", "darkgreen", "darkred"), 
+             palette = c("#F6Be00", "darkblue", "#F6Be00",  "#552586", "grey", "darkgreen", "darkred"), 
              title = "surface water")
 
 
@@ -167,7 +473,7 @@ pc <- psych::principal(SWgrab_allsites_dat[, -1], 4,rotate="varimax") #have to c
 print(pc$scores[1:5,])  # Scores returned by principal()
 
 #compare to no rotation 
-pc2 <- psych::principal(SWgrab_allsites_dat[, -1], 4,rotate="none") #have to chose the # of components to extract 
+pc2 <- psych::principal(SWgrab_allsites_dat[, -1], 5,rotate="none") #have to chose the # of components to extract 
 print(pc2$scores[1:5,])  # Scores returned by principal()
 
 print(pc$loadings)
